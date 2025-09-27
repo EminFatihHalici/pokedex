e088c06bf9422056bf5a3b42e5d24ae6dba04976
Emin Fatih Halici <halicieminfatih@gmail.com>
2025-09-27 07:08:09 +0200

    feat: adding for loop to get the index of the pokemon

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
+
 function onloadFunc() {
     loadPokemon();
 }
 
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
-
-async function loadPokemon() {
+async function loadPokemon() {  //fetche basis url nur mit den ersten 20
     try {
-
         let response = await fetch(BASE_URL);
         let data = await response.json();
-        console.log(data);
+        let pokemonList = data.results;
+        console.log("List loaded:", data);
+
+        for (let i = 0; i< pokemonList.length; i++) {
+            let pokemon = pokemonList[i];
+            renderPokemonCard(pokemon);
+        }
+        
     } catch (error) {
         console.error("Failure", error);
+        return [];
     }
 }

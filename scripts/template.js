5a414ef702570cb8bc695d561fc42e6ccecf625a
Emin Fatih Halici <halicieminfatih@gmail.com>
2025-10-07 10:55:57 +0200

    fix: made it final responsive

--- scripts/template.js
+++ scripts/template.js
@@ -17,7 +17,7 @@ function getPokemonCardHTML(p) {
 function buildBigCardHTML({id,name,imgUrl,bgColor,typeHtml,statsHtml}){
     return `
        <div id="bigCardContainer" class="big-card-container" onclick="closeBigCard(event)">
-                <div class="card shadow-lg p-4 text-center position-relative mx-auto" style="background-color:${bgColor}; max-width:600px;width:100%;">
+                <div class="card  shadow-lg p-4 text-center position-relative mx-auto big-card-res" style="background-color:${bgColor}; max-width:600px;width:100%;">
                     <span class="pokemon-id position-absolute top-0 start-0 m-2">#${id}</span>
                     <button class="btn btn-dark btn-sm position-absolute top-0 end-0 m-2" onclick="closeBigCard()">X</button>
                     <img class="pokemon-image mx-auto d-block mb-3" src="${imgUrl}" alt="${name}" style="max-height:250px;">
